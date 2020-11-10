78b1fca97bc8b917a5fa4acb7dbe5789db08406f
Júlia Barboza Brunelli <69260646+Mikix30@users.noreply.github.com>
2020-11-10 19:04:41 -0300

    Feat: Adiciona Atividade 04

---
+++ atividade4.py
@@ -0,0 +1,26 @@
+numb1 = int(input("Qual o número desejado? "))
+
+numb2 = int(input("Qual o outro número desejado? "))
+
+numb3 = int(input("Qual o outro número desejado? "))
+
+numb4 = int(input("Qual o outro número desejado? "))
+
+numb5 = int(input("Qual o outro número desejado? "))
+
+numb6 = int(input("Qual o outro número desejado? "))
+
+numb7 = int(input("Qual o outro número desejado? "))
+
+numb8 = int(input("Qual o outro número desejado? "))
+
+numb9 = int(input("Qual o outro número desejado? "))
+
+numb10 = int(input("Qual o outro número desejado? "))
+
+
+listt = [numb1,numb2,numb3,numb4,numb5,numb6,numb7,numb8,numb9,numb10]
+
+
+one = filter(lambda x: '%2==0' in x, print("é par: ",[listt]))  
+two = filter(lambda x: '%!=0' in x, print("é ímpar: ",[listt]))
