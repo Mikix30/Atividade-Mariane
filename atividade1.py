059255215c75353034d93fbe94403baa961a0901
Júlia Barboza Brunelli <69260646+Mikix30@users.noreply.github.com>
2020-11-10 18:34:25 -0300

    Feat: Adiciona Atividade 01

---
+++ atividade1.py
@@ -0,0 +1,7 @@
+agename = lambda nome, idade: print(scriptname,"tem",scriptage,"anos")
+
+scriptname = str(input("Qual seu nome? "))
+
+scriptage = int(input("Qual sua idade? "))
+
+print(agename(scriptname, scriptage))
